ec6c59e20445de75a65bd42e30e4376f046afc4b
Gil0 <gilrafael47@gmail.com>
2017-04-25 23:04:20 -0500

    Modificacion de rutas haciami equipo

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -25,7 +25,12 @@
 	            </ul>
 	        </li>
 			<li><a class="letternav" href="{{url ('/personal/projects')}}">Proyecto</a></li>
-			<li><a class="letternav" href="#">Mi equipo</a></li>
+			<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Mi Equipo<b class="caret"></b></a>
+      			<ul class="dropdown-menu">                				
+        			<li><a href="{{url('/corporation/myteam/'.Auth::user()->id)}}">Mi Personal</a></li>
+        			<li><a href="{{url('/corporation/newpersonal/'.Auth::user()->id)}}">Nuevo Personal</a></li>
+      			</ul>
+    		</li>		
 		@else
 			<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Convocatoria<b class="caret"></b></a>
 	            <ul class="dropdown-menu">
