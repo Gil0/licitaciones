759dcd0469009ef67d3cce7d02db802f99a62ada
Gil0 <gilrafael47@gmail.com>
2017-04-25 23:01:53 -0500

    Informacion que mostrara la vista  mi Personal

--- resources/views/Corporation/myteam.blade.php
+++ resources/views/Corporation/myteam.blade.php
@@ -17,31 +17,41 @@
 <div class="navar">
 	<ul class="nav nav-pills">
 		<li><a class="letternav" href="{{url('/corporation/dashboard')}}">Inicio</a></li>
-		<li><a class="letternav" href="#">Convocatoria</a></li>
-		<li><a class="letternav" href="#">Proyecto</a></li>
-		<li><a class="letternav" href="{{url ('/corporation/myteam')}}">Mi equipo</a></li>
+		<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Convocatoria<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="{{url('/announcements')}}">General</a></li>
+				<li class ="MisConvocatorias" value="{{Auth::user()->id}}"><a >Mis convocatorias</a></li>
+            </ul>
+        </li>
+		<li><a class="letternav" href="{{url ('/corporation/projects')}}">Proyecto</a></li>
+		<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Mi Equipo<b class="caret"></b></a>
+      <ul class="dropdown-menu">                				
+        <li><a href="{{url('/corporation/myteam/'.Auth::user()->id)}}">Mi Personal</a></li>
+        <li><a href="{{url('/corporation/newpersonal/'.Auth::user()->id)}}">Nuevo Personal</a></li>
+      </ul>
+    </li>
 	</ul>
 </div>
-<h1>My Team</h1>
 
 <div class="container">
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
-      <h2>Personal</h2>
+      <h2>Mi Personal</h2>
       <div class="panel panel-default">
         <table class="table table-striped">
           <thread>
             <tr>              
               <th class="head"><h5>Nombre</h5></th>
               <th class="head"><h5>E-mail</h5></th>
-              <th class="head"></th> 
+              <th class="head">Telefono</th> 
             </tr>
           </thread>
           <tbody>
           @foreach($personal as $personal)
             <tr class="rowsTabla">              
               <th >{{$personal->name}}</th>
-              <th >{{$personal->email}}</th>              
+              <th >{{$personal->email}}</th> 
+              <th >{{$personal->phoneNumber}}</th>              
             </tr>
             @endforeach
           </tbody>
@@ -50,13 +60,12 @@
     </div>
   </div>
 </div>
-
-
-<h1>Nuevo Personal</h1>
-<button id="botonAgregar" class="btn btn-info">Agregar Personal</button>
-<script type="text/javascript">
-    document.getElementById("botonAgregar").onclick = function () {
-        location.href = "/corporation/dashboard";
-    };
+<script>
+  $(document).ready(function(){
+	  $('li.MisConvocatorias').click(function(){
+		 window.location.href = '/corporation/dashboard/misConvocatorias/' +$(this).attr('value');
+		 
+	  });
+  });
 </script>
 @endsection
