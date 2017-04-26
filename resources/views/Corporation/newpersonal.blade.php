a5e7e08151801d6d074f3911d1ff779b681eaf30
Gil0 <gilrafael47@gmail.com>
2017-04-25 23:02:33 -0500

    Informacion que mostrara la vista nuevo Personal

--- resources/views/Corporation/newpersonal.blade.php
+++ resources/views/Corporation/newpersonal.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+@section('content') 
 <style>
 	@import url('https://fonts.googleapis.com/css?family=Anton');
     @import url('https://fonts.googleapis.com/css?family=Oswald');
@@ -17,31 +17,55 @@
 <div class="navar">
 	<ul class="nav nav-pills">
 		<li><a class="letternav" href="{{url('/corporation/dashboard')}}">Inicio</a></li>
-		<li><a class="letternav" href="#">Convocatoria</a></li>
-		<li><a class="letternav" href="#">Proyecto</a></li>
-		<li><a class="letternav" href="{{url ('/corporation/myteam')}}">Mi equipo</a></li>
+		<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Convocatoria<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="{{url('/announcements')}}">General</a></li>
+				<li class ="MisConvocatorias" value="{{Auth::user()->id}}"><a >Mis convocatorias</a></li>
+            </ul>
+        </li>
+		<li><a class="letternav" href="{{url ('/corporation/projects')}}">Proyecto</a></li>
+		<li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Mi Equipo<b class="caret"></b></a>
+      <ul class="dropdown-menu">                				
+        <li><a href="{{url('/corporation/myteam/'.Auth::user()->id)}}">Mi Personal</a></li>
+        <li><a href="{{url('/corporation/newpersonal/'.Auth::user()->id)}}">Nuevo Personal</a></li>
+      </ul>
+    </li>
 	</ul>
 </div>
-<h1>My Team</h1>
 
 <div class="container">
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
-      <h2>Personal</h2>
+      <h2>Nuevo Personal</h2>
       <div class="panel panel-default">
-        <table class="table table-striped">
+        <table class="table table-hover" class="rowsTabla">
           <thread>
             <tr>              
-              <th class="head"><h5>Nombre</h5></th>
-              <th class="head"><h5>E-mail</h5></th>
-              <th class="head"></th> 
+              <th class="head text-center"><h5>Nombre</h5></th>
+              <th class="head text-center"><h5>E-mail</h5></th>              
+              <th class="head text-center"><h5>Telefono</h5></th> 
+              <th class="head text-center"><h5>Area</h5></th> 
+              <th class="head text-center"><h5>Direccion</h5></th> 
+              <th class="head text-center"><h5>RFC</h5></th>               
+              <th class="head text-center"><h5>Estado</h5></th> 
             </tr>
           </thread>
           <tbody>
-          @foreach($personal as $personal)
+          @foreach($personal as $personal)          
             <tr class="rowsTabla">              
-              <th >{{$personal->name}}</th>
-              <th >{{$personal->email}}</th>              
+              <th class="text-center">{{$personal->name}}</th>
+              <th class="text-center">{{$personal->email}}</th>              
+              <th class="text-center">{{$personal->phoneNumber}}</th>
+              <th class="text-center">{{$personal->area}}</th>
+              <th class="text-center">{{$personal->address}}</th>
+              <th class="text-center">{{$personal->rfc}}</th>              
+              <th class="text-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default" style="width:100%;" id="{{$personal->id}}" value="{{$personal->idCorporation}}">
+                    <i class="fa fa-bullseye" aria-hidden="false">Agregar</i>
+                    </button>
+                </div>
+              </th>              
             </tr>
             @endforeach
           </tbody>
@@ -50,13 +74,50 @@
     </div>
   </div>
 </div>
+<script>
+    $(document).ready(function(){
 
+      $('.rowsTabla>th>div>button').each(function(){
+             if($(this).attr('value') == null){
+                 $(this).addClass("btn btn-danger");
+             }
+             else{
+                 $(this).addClass("btn-success");
+             }
+         });
 
-<h1>Nuevo Personal</h1>
-<button id="botonAgregar" class="btn btn-info">Agregar Personal</button>
-<script type="text/javascript">
-    document.getElementById("botonAgregar").onclick = function () {
-        location.href = "/corporation/dashboard";
-    };
+         $('.rowsTabla > th > div > button').click(function(){
+             //alert($(this).attr('id'));
+             if($(this).attr('value') == 0)
+             {
+                 $(this).removeClass('btn-danger');
+                 $(this).addClass('btn-success');
+                 $(this).attr('value',1);
+             }
+             else{
+                 $(this).removeClass('btn-success');
+                 $(this).addClass('btn-danger');
+                 $(this).attr('value',0);
+             }
+             $.ajax({
+                 url:'/corporation/newpersonal'+$(this).attr("id")+'/cambiarStatus',
+                 type:'POST',
+                 dataType:'json',
+                 data:{
+                     'idCorporation': $(this).attr('value')
+                 },beforeSend: function (xhr) {                                      //Antes de enviar la peticion AJAX se incluye el csrf_token para validar la sesion.
+                    var token = $('meta[name="csrf_token"]').attr('content');
+
+                    if (token) {
+                        return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+                    }
+                },
+                 success:function(response){
+                     //alert(response);
+                 }
+             });
+         });        
+    });
+  
 </script>
 @endsection
