ef826ad4685d6fc5fe59e655a9688b805b369ffd
Jcarlos-rl <carlos_-619@hotmail.com>
2017-05-01 00:36:43 -0500

    Solucionando problema del nav al cerrar la ventana

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,6 +132,37 @@
         padding-top: 5px;
     }
 </style>
+@if(!Auth::guest())
+@if(Auth::user()->role == 'Corporation')
+<div class="navar">
+    <ul class="nav nav-pills">
+        <li><a class="letternav" href="{{url ('/home')}}">Inicio</a></li>
+            <li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Convocatoria<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                     <li><a href="{{url('/announcements')}}">General</a></li>
+                     <li class ="MisConvocatorias" value="{{Auth::user()->id}}"><a >Mis convocatorias</a></li>
+                </ul>
+            </li>
+        <li class="MisProyectos" value="{{Auth::user()->id}}"><a class="letternav">Proyecto</a></li>
+        <li><a class="letternav" href="#">Mi equipo</a></li>
+    </ul>
+</div>
+@else
+<div class="navar">
+    <ul class="nav nav-pills">
+        <li><a class="letternav" href="{{url ('/home')}}">Inicio</a></li>
+        <li><a href="#" class="letternav dropdown-toggle" data-toggle="dropdown">Convocatoria<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="{{url('/announcements')}}">General</a></li>
+                <li class ="MisConvocatorias" value="{{Auth::user()->id}}"><a >Mis convocatorias</a></li>
+            </ul>
+        </li>
+        <li class="MisProyectos" value="{{Auth::user()->id}}"><a class="letternav">Proyecto</a></li>
+    </ul>
+</div>
+@endif
+@else
+@endif
 <div class="">
     <div class="col-sm-12 banner">
         <img class="imgbanner" src="Imagenes/banner.png">
